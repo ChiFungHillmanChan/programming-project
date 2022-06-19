2f76fa5914fb46c0400e750465a49644e8e7bdad
ChiFungHillmanChan <sc21cfhc@leeds.ac.uk>
2022-06-19 18:14:36 +0100

    minor changes

---
+++ python-project/check-palindrome.py
@@ -0,0 +1,16 @@
+def main(words):
+    #check if words is alpha and transform to lower case
+    answer = "".join(x for x in words if x.isalpha()).lower()
+
+    # [::-1] is to change the order of the words
+    # list[<start index> : <stop index> : <step>]
+    if answer == answer[::-1]:
+        print("True")
+        return True
+    else:
+        print("False")
+        return False
+
+
+main("A man, a plan, a canal: Panama")
+
---
+++ python-project/valid-subsequence.py
@@ -0,0 +1,18 @@
+def main(array, sequence):
+    place = 0
+    for value in array:
+        if place == len(sequence):
+            break
+        if sequence[place] == value:
+            place += 1
+
+    
+    print(place == len(sequence))
+    return place == len(sequence)
+
+#correct
+main([5,1,22,25,6,-1,8,10],[5,1,6,22])
+#wrong order
+main([5,1,22,25,6,-1,8,10],[5,1,22,6])
+#repeated sequence
+main([5,1,22,25,6,-1,8,10],[5,1,22,22,6])
