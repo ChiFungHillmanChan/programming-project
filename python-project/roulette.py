b77c4758a7626f6c6674145534889aefefd81b9d
ChiFungHillmanChan <sc21cfhc@leeds.ac.uk>
2022-06-20 01:29:10 +0100

    Update roulette.py

--- python-project/roulette.py
+++ python-project/roulette.py
@@ -1,5 +1,8 @@
 import random 
 import time
+import os 
+#define time to spin
+TIME = 3
 
 #define roulette 
 def roulette(option):
@@ -8,7 +11,7 @@ def roulette(option):
     check = 0
     color = ["red", "black"]
     choice = ["small", "big"]
-    if option == '1':
+    if option == 2:
         roulette.update({'00':('00', "green", "none")}) 
     
     for x in range(1, 37):
@@ -16,18 +19,67 @@ def roulette(option):
         change = 1 if change == 0 else change-1
         check = 1 if x >= 17 else check
         roulette.update(case)
+    
 
     return roulette
+    # ways to get value in key
+    # if option in roulette.keys()
+    # print option[0] or [1]
 
 #detect win 
-def detect_win(guess, option):
-    option_list = { 'red', 'black', 'odd', 'even' , 'big', 'small' }
-    if option in option_list:
-        pass
+def detect_win(option, bet, roulette_choice, run):
+    option_list = {'red', 'black', 'odd', 'even' , 'big', 'small' }
+    spin = random.randint(0, (len(roulette_choice.keys())-1))
+    if(spin > 1):
+        num = list(roulette_choice.values())[spin][0]
+        color = list(roulette_choice.values())[spin][1]
+        check = list(roulette_choice.values())[spin][2]
     else:
-        pass
+        num = list(roulette_choice.values())[spin][0]
+        color = list(roulette_choice.values())[spin][1]
+        check = 'None'
+
+    print(f'Result: ******** {num} | {color} | {check} ********')
+
+    if option == list(roulette_choice.keys())[spin] and (option == '0' or option == '00'):
+        print("        ******** YOU WIN ********")
+        return bet * 36
+    elif option in option_list:
+        #check red black big small
+        if option == list(roulette_choice.values())[spin][1]:
+            print("        ******** YOU WIN ********")
+            return bet * 2
+        elif option == list(roulette_choice.values())[spin][2]:
+            print("        ******** YOU WIN ********")
+            return bet * 2
+        #check odd even
+        elif list(roulette_choice.values())[spin][0] != '00':
+            if int(list(roulette_choice.values())[spin][0]) % 2 == 0:
+                if option == 'even':
+                    print("        ******** YOU WIN ********")
+                    return bet * 2
+                else:
+                    print("        ******** YOU LOSE ********")
+                    return -bet 
+            else:
+                if option == 'odd':
+                    print("        ******** YOU WIN ********")
+                    return bet * 2
+                else:
+                    print("        ******** YOU LOSE ********")
+                    return - bet 
+        else:
+            print("        ******** YOU LOSE ********")
+            return -bet 
+    elif option == list(roulette_choice.keys())[spin]:
+            print("        ******** YOU WIN ********")
+            return bet * 36
+    else:
+        print("        ******** YOU LOSE ********")
+        return -bet 
+
 #count every percentage
-def percentage():
+def percentage(total, win):
     pass
 
 #rule
@@ -36,27 +88,65 @@ def show_rule(option):
         print("RULES: ")
         print(" ")
 
-#main 
-def main():
-    time_count = 20
-    print("Start for the roulette\n")
-    print("Ender 1 for American Roulette(with 00)")
-    print("Enter 2 for European Roulette(without 00)\n ")
-    option = input("Enter which roulette you want to play: ")
 
-    roulette(option)
+# time counter
+def countdown():
+    t = TIME
+    while t:
+        time.sleep(1)
+        t -= 1
+
+def auto_start():
+    t = TIME
+    while t >= 0:
+        try:
+            seconds = divmod(t, 60)
+            timer = '{:d}'.format(seconds[1])
+            print(f'Auto Start in {timer}, click "Ctrl+C" to stop', end="\r")
+            time.sleep(1)
+            t -= 1
+        except KeyboardInterrupt:
+            return False
+    return True
+# clear system
+def clear():
+    os.system('clear')
+
+#main
+def main():
+    print("Start for the roulette\n")
+    while True:
+        print("Enter 1 for European Roulette(without 00)")
+        print("Ender 2 for American Roulette(with 00)\n")
+        run = int(input("Enter which roulette you want to play: "))
+        if run != 1 and run != 2:
+            clear()
+            print('Invalid Option!Place choose again!')
+        else:
+            break
+
+    roulette_choice = roulette(run)
 
     print("Roulette is set! Time to guess!! good luck! \n")
 
-    Deposit = input ("Enter your Deposit: ")
+    deposit = int(input("Enter your Deposit: "))
+    clear()
+    while deposit > 0:
+        print(f'Your deposit: {deposit}')
+        if auto_start():
+            try:
+                bet , option = [x for x in input("\nBets by format(amount choice): ").split(" ")]
+            except ValueError:
+                print("Pleace input 2 value with correct format [amount option]\n")
+                continue
+            if int(bet) > deposit: 
+                print('Not enough deposit\n')
+                continue
+            deposit += int(detect_win(option.lower(), int(bet), roulette_choice, run-1))
+            countdown()
+            clear()
+        else:
+            break
 
-    while Deposit > 0:
-        option = input("Keep playing? Type 'Y' to continue. ")
-
-        if option.lower() != 'y':
-            print("Thank you for playing !")
-            quit()
-        
-            
-
+    print(f"\nYour final deposit is {deposit}, Thank you for playing roulette!")
 main()
