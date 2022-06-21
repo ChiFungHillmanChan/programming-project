f16f8886128d9353d54abb1004e1e9499fb3061c
ChiFungHillmanChan <sc21cfhc@leeds.ac.uk>
2022-06-21 01:22:05 +0100

    update

--- python-project/README.md
+++ python-project/README.md
@@ -45,3 +45,44 @@ subsequence:[2 ,2, 3]
 
 # This will return False as subsequence has two '2's but main has only one 
 ```
+
+## roulette.py
+
+This is a fun project to play with Roulette. Player can input the deposit and start the game.
+
+This function imports time and random. 
+```python
+import time
+import random
+```
+
+the roulette will auto spin after required TIME. But player can use keyboard to interrupt it
+
+The function is not fully finished yet. It is functionable but still can be improved
+
+## ugly_number.py
+
+requirement for ugly number:
+
+Programming challenge description:
+Credits: This challenge has appeared in a google competition.
+
+Once upon a time in a strange situation, people called a number ugly if it was divisible by any of the one-digit primes (2, 3, 5 or 7). Thus, 14 is ugly, but 13 is fine. 39 is ugly, but 121 is not. Note that 0 is ugly. Also note that negative numbers can also be ugly; -14 and -39 are examples of such numbers.
+
+One day on your free time, you are gazing at a string of digits, something like:
+123456
+You are amused by how many possibilities there are if you are allowed to insert plus or minus signs between the digits. For example you can make:
+
+1 + 234 - 5 + 6 = 236
+which is ugly. Or
+123 + 4 - 56 = 71
+which is not ugly.
+
+It is easy to count the number of different ways you can play with the digits: Between each two adjacent digits you may choose put a plus sign, a minus sign, or nothing. Therefore, if you start with D digits there are 3^(D-1) expressions you can make. Note that it is fine to have leading zeros for a number. If the string is '01023', then '01023', '0+1-02+3' and '01-023' are legal expressions.
+
+Your task is simple: Among the 3^(D-1) expressions, count how many of them evaluate to an ugly number.
+Input:
+Your program should read lines of text from standard input. Each line will consist of a non-empty string containing only the characters '0' through '9'. Each string is no more than 13 characters long.
+Output:
+For each input string, print to standard output the number of expressions that evaluate to an ugly number, one number per line.
+
---
+++ python-project/ugly_number.py
@@ -0,0 +1,90 @@
+#check ugly numbers
+ugly_list = ['2','3','5','7']
+sign = ['', '+', '-']
+
+def import_number(input_string):
+    #variables
+    count_ugly_number = 0
+    split_input = [a for a in str(input_string)]
+    checklist = []
+    count_power_number = len(split_input) - 1
+
+    # if only one digit number, directly check and return 
+    if int(count_power_number) == 0:
+        return check_ugly_requirement(input_string)
+
+    # append a checklist for later calculation
+    # 0 means space, 1 means plus sign, 2 means minus sign
+    for i in range(count_power_number):
+        checklist.append(0)
+
+    #There are 3^(d-1) expression so loop for 3^(d-1) times
+    for j in range(3 ** count_power_number):
+
+        # create an expression list
+        # expression list format : ['number', 'sign', 'number', 'sign', ....]
+        expression = []
+        expression.append(split_input[0])
+
+        # add sign to expression list
+        for x in range(count_power_number):
+            if x == -1:
+                break
+            if checklist[x-1] == 0:
+                expression.append(sign[0])
+            elif checklist[x-1] == 1:
+                expression.append(sign[1])
+            elif checklist[x-1] == 2:
+                expression.append(sign[2])
+            expression.append(split_input[x+1])
+        
+        # this section is to change the checklist
+        checklist[-1] = int(checklist[-1]) + 1
+        for k in reversed(range(len(checklist))):
+            try:
+                if checklist[k] == 3:
+                    checklist[k] = 0
+                    checklist[k-1] = int(checklist[k-1]) + 1
+            except IndexError:
+                continue
+        
+        # as 01 is invalid. The 0 must be removed if the number is started from 0 
+        for idx in range(len(expression)):
+            if idx == len(expression) -1:
+                break
+            try:
+                if expression[idx] == '0' and expression[idx+1] == '':
+                    expression.pop(idx)
+                elif expression[idx] == '0' and int(expression[idx+1]).isnumeric() and not int(expression[idx+1]).isnumeric():
+                    expression.pop(idx)
+            except ValueError:
+                continue
+        # join the expression list to become a string
+        join_expression = ''.join(expression)
+        # use eval to convert the string to an equation 
+        count_ugly_number += check_ugly_requirement(eval(join_expression))
+
+    return count_ugly_number
+
+def check_ugly_requirement(expression):
+    #check if the expression is an ugly number
+    # check if it is divisable by the ugly number list
+    for x in ugly_list:
+        if int(expression) % int(x) == 0:
+            return 1
+    return 0
+
+def main():
+    #input
+    input_string = input('Ender a string:')
+
+    #check requirement
+    if not input_string.isnumeric() or len(input_string) > 13:
+        print ("Invalid input string!")
+        quit()
+
+    #run the program and print the result
+    print(import_number(input_string))
+
+
+main()
