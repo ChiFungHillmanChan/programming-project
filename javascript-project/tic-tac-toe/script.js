420e46cde0bcaa1d10148c513c5e39d2064eb8f9
ChiFungHillmanChan <sc21cfhc@leeds.ac.uk>
2022-06-25 15:58:28 +0100

    create tic-tac-toe in javascript

---
+++ javascript-project/tic-tac-toe/script.js
@@ -0,0 +1,96 @@
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const CIRCLE_CLASS = 'circle'
+const X_CLASS =  'x'
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const restartButton = document.getElementById('restartButton')
+
+const playerTurnTextMessage = document.getElementById('player_turn')
+
+const WINNING_COMBINATION = [
+    [0,1,2], 
+    [3,4,5], 
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+let circleTurn 
+
+startGame()
+restartButton.addEventListener('click', startGame)
+
+
+function startGame(){
+    circleTurn = false
+    playerTurnTextMessage.innerHTML = `${circleTurn ? "O's" : "X's"} Turn!`
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
+}
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if(checkWin(currentClass)){
+        endGame(false)
+    }
+    else if (isDraw()){
+        endGame(true)
+    }
+    else{
+        swapTurns()
+        setBoardHoverClass()
+    }   
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass)
+}
+
+function swapTurns(){
+    circleTurn = !circleTurn
+    playerTurnTextMessage.innerHTML = `${circleTurn ? "O's" : "X's"} Turn!`
+}
+
+function endGame(draw){
+    if (draw){
+        winningMessageTextElement.innerText = 'Draw!'
+    }
+    else{
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn){
+        board.classList.add(CIRCLE_CLASS)
+    }
+    else{
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATION.some(combination => {
+        return combination.every(index =>{
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
+
+function isDraw(){
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS) 
+    })
+}
